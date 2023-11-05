0d1eefc211f0538c0fd4ef884e4aabfffa73d360
Alpha222005826 <149673359+Alpha222005826@users.noreply.github.com>
2023-11-05 03:45:09 -1200

    Add files via upload

---
+++ forexample.java
@@ -0,0 +1,10 @@
+//creating program that count from 1 to 10
+public class forexample { //creatin class for example 
+public static void main(String[] args) {  
+      
+ System.out.println("counting from 1 to 10");
+for(int i=1;i<=5;i++){//declaring for 
+  System.out.println(i);//out that will display that numbers
+}//end for
+  }  
+}
---
+++ forloop.java
@@ -0,0 +1,13 @@
+//using for loop to  display five different names of top five student
+
+public class forloop  {
+        public static void main(String[] args) {  
+            //Declaring an array  
+         System.out.println("list of Top five student in class");
+    String NAME[]={"NDAHIRO","KALISA","VALENCE","CLAUDE","EMMAULE"};  
+    //Printing array using for-each loop  
+    for(String i:NAME){  
+        System.out.println(i);  
+    }//end  for statement
+}  
+}//end of class
