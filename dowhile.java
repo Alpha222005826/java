033e40c654b095155d19e3364c760b438686706d
Alpha222005826 <149673359+Alpha222005826@users.noreply.github.com>
2023-11-05 04:38:37 -1200

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+// displaying word welcome several times
+public class dowhile{//creating class called dowhile
+
+public static void main(String[] args) {//main method
+int h=1;//initializing value
+do{//declarind do while loop
+System.out.println("Welcome");//displaying message
+h++;}
+while(h<=10);
+}  //end of dowhile
+}  
+
