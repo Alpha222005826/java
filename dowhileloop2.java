87cf9b66d1ec8982aadce70a85e16df89cbcb4e9
Alpha222005826 <149673359+Alpha222005826@users.noreply.github.com>
2023-11-06 07:10:40 -1200

    Add files via upload

---
+++ dowhileloop2.java
@@ -0,0 +1,14 @@
+// creating loop that will display even numbers between 2 to 10
+
+public class dowhileloop2 {
+    public static void main(String args[]){
+        int x=2;
+       do { System.out.println(x);
+            x=x+2;
+       }
+        while(x<=10);// declaring while loop
+           
+        }//end loop
+        
+    
+    }
