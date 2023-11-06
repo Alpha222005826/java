a1a323f03c21ae7d8e009459a06a8d19af4c56ed
Alpha222005826 <149673359+Alpha222005826@users.noreply.github.com>
2023-11-06 04:28:48 -1200

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,11 @@
+//creating infit loop using while loop
+public class whileloop {//creating class whileloop
+    public static void main(String args[]){//main metho
+        int password=15;//initializing
+        while (password>=10){ // declaring while loop
+                  System.out.println("allowed");//output
+        }//end while loop
+        password--;
+        }
+        } 
+
